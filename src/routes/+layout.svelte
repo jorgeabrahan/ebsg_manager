35a5c7e6ee92bc7a08351c39b3b750d81f701684
jorgeabrahan <jorge24abrahan@gmail.com>
2025-03-20 17:43:51 -0600

    Implement preserve session functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,41 @@
 <script lang="ts">
-	import { Toaster } from 'svelte-sonner';
-	import '../app.css';
-	let { children } = $props();
+  import { Toaster } from 'svelte-sonner';
+  import '../app.css';
+  import { onMount } from 'svelte';
+  import { ServiceAuth } from '$lib/services/service_auth';
+  import { goto } from '$app/navigation';
+  import { isUserBeingFetched, user } from '$lib/stores/store_user';
+  let { children } = $props();
+
+  onMount(() => {
+    $isUserBeingFetched = true;
+    (async () => {
+      const { data } = await ServiceAuth.getUser();
+      $user = data;
+      $isUserBeingFetched = false;
+      const isAuthenticated = data != null && data?.role === 'authenticated';
+      const isDashboardPath = window.location.pathname === '/dashboard';
+      const isAuthPath =
+        window.location.pathname === '/sign-in' ||
+        window.location.pathname === '/sign-up';
+      if (!isAuthenticated && isDashboardPath) {
+        goto('/sign-in');
+        return;
+      }
+      if (isAuthenticated && isAuthPath) {
+        goto('/dashboard');
+        return;
+      }
+      if (!isDashboardPath && !isAuthPath) {
+        if (isAuthenticated) {
+          goto('/dashboard');
+          return;
+        } else {
+          goto('/sign-in');
+        }
+      }
+    })();
+  });
 </script>
 
 <Toaster richColors />
